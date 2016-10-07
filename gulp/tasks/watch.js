d3ada8476f64276df11e7f53924a445866d86228
Liren <gliren.business@gmail.com>
2016-10-07 12:30:03 -0500

    large hero and intro block completed.

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'), 
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('watch', function(){
+   
+    browserSync.init({
+       notify: false,
+       server:{
+           baseDir: "app"
+       } 
+    });
+   
+    watch('./app/index.html', function(){
+        
+        browserSync.reload();
+    });
+    
+     watch('./app/assets/styles/**/*.css', function(){
+                /* **: any folders in between; *: any files end with .css */
+        
+        gulp.start('cssInject');
+    });
+    
+});
+
+                     /* 3. run the styles task first/convert to browser readable code */
+gulp.task('cssInject', ['styles'], function(){
+    gulp.src('./app/temp/styles/styles.css') /* 1. locate the css file */
+    .pipe(browserSync.stream()); /* 2. browsersync it */
+});
