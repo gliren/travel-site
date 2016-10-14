e4c624fb9bd70a50aa86473a59ab822713101104
Liren <gliren.business@gmail.com>
2016-10-14 13:00:54 -0500

    Completed reveal on scroll.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
   // can be any name           no .js
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
-    // object 
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
